f6eef15fb39eba1d41c96c33ea382ed1728441d6
Dewey Dunnington <dewey@fishandwhistle.net>
2024-10-16 23:21:04 -0500

    use loader

--- R/adbc.R
+++ R/adbc.R
@@ -1,5 +1,8 @@
 
 sf_load_adbc <- function(driver_name, entrypoint, version, driver, error) {
     message("Loading!")
-    5
+    tryCatch(
+        adbcdrivermanager::adbc_driver_load(driver_name, entrypoint, version, driver, error),
+        error = function(...) 5L
+    )
 }
